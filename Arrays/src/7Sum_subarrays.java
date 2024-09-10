97dd2a4daa0e8997d4369a1856b7d3a9d0fa0063
NAVEENKUMAR REDDY K <naveen9019793837@gmail.com>
2024-09-10 12:10:18 +0530

    PRINT_sSUM_OF_SUBARRAYS

--- Arrays/src/7Sum_subarrays.java
+++ Arrays/src/7Sum_subarrays.java
@@ -1,27 +1,28 @@
+
 public class Sum_subarrays {
 
 
 
-   public static void subarrays(int numbers[]) {
-       for (int i = 0; i < numbers.length; i++) {
-        // int start=i;
-            for(int j=i;j<numbers.length;j++){
-                // int end=j;
-                int sum=0;
-                for (int k = i/*start */; k <=j/*end */; k++) {
+    public static void subarrays(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+         // int start=i;
+             for(int j=i;j<numbers.length;j++){
+                 // int end=j;
+                 int sum=0;
+                 for (int k = i/*start */; k <=j/*end */; k++) {
+                    
+                    sum=sum+numbers[k];
                    
-                   sum=sum+numbers[k];
-                  
-                }
-                System.out.print("["+sum+"]");
-                System.out.print(" ");
-            }
-            System.out.println("");
-           
-       }
-   }
-    public static void main(String[] args) {
-        int number[]={1,2,3,4,5};
-        subarrays(number);
+                 }
+                 System.out.print("["+sum+"]");
+                 System.out.print(" ");
+             }
+             System.out.println("");
+            
+        }
     }
-}
+     public static void main(String[] args) {
+         int number[]={1,2,3,4,5};
+         subarrays(number);
+     }
+ }
