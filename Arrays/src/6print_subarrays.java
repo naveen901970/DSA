d4d9eea6b745cac95980c510b80edb3c764f3f14
NAVEENKUMAR REDDY K <naveen9019793837@gmail.com>
2024-09-10 09:38:48 +0530

    PRINT_SUBARRAYS

---
+++ Arrays/src/6print_subarrays.java
@@ -0,0 +1,23 @@
+public class print_subarrays {
+
+
+
+   public static void subarrays(int numbers[]) {
+       for (int i = 0; i < numbers.length; i++) {
+        // int start=i;
+            for(int j=i;j<numbers.length;j++){
+                // int end=j;
+                for (int k = i/*start */; k <=j/*end */; k++) {
+                    System.out.print("["+numbers[k]+"]");
+                }
+                System.out.print(" ");
+            }
+            System.out.println("");
+           
+       }
+   }
+    public static void main(String[] args) {
+        int number[]={1,2,3,4,5};
+        subarrays(number);
+    }
+}
